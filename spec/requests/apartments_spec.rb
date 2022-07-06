39e04d9f55202249281ebf5cf03d2947444acb57
Renzlawr <RSLogan23@gmail.com>
2022-07-06 16:54:28 -0600

    added some values in apartments spec

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
     it 'returns a list of apartments' do
     
-      user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+      user = User.where(email: 'test1@gmail.com').first_or_create(password: '123123', password_confirmation: '123123')
 
       user.apartments.create(
-        street: string,
-        city: string,
-        state: string,
-        manager: string,
-        email: string, 
-        price: string, 
-        bedrooms: integer, 
-        bathrooms: integer, 
-        pets: string,
-        image: text
+        street: "2942 ratopolis lane",
+        city: "Los Angeles",
+        state: "California",
+        manager: "Rat Lord",
+        email: "biggestratwhoeatsallofthecheese@gmail.com", 
+        price: "10 blocks of swiss", 
+        bedrooms: "5", 
+        bathrooms: "3", 
+        pets: "no pets",
+        image: "https://bloximages.chicago2.vip.townnews.com/ladowntownnews.com/content/tncms/assets/v3/editorial/3/82/3824715a-a4f7-11e9-a9ea-73ab3cf22228/5d290e93b22c8.image.jpg"
       )
 
       get '/apartments'
